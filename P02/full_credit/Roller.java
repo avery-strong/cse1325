7f0ce795f380a081b5235183c598c196ce770e15
Avery Strong <avery.strong@mavs.uta.edu>
2021-09-04 01:58:52 -0500

    fourth commit

--- P02/full_credit/Roller.java
+++ P02/full_credit/Roller.java
@@ -1,33 +1,24 @@
 import java.util.Arrays;
 import java.lang.Math;
-/*
-	In your git-managed cse1325 directory cse1325/P02/full_credit 
-	(capitalization matters!), create a Java program in file Roller.java 
-	that accepts exactly two command-line parameters: (1) the number of 
-	dice to roll and (2) the number of faces per die.
+import java.lang.*;
 
-	Create an array of ints and fill with the specified die rolls. 
-	Consider Math.random() to generate the die rolls - it returns a float 
-	between 0.0 and 1.0.
-
-	Sort the array of ints using the function¹ Arrays.sort(array). 
-	To do this, you'll need to first import java.util.Arrays.
-
-	Then print each die roll separated by spaces, followed by the sum and 
-	average of the rolls.
-	You don't need to worry about error handling yet, as we'll cover that
-	topic in the near future.
-
-	If two command-line parameters are not provided, print to standard error 
-	"usage: java Roll [#dice] [#sides]" and exit.
-
-	Add, commit, and push all files to your private cse1325 GitHub repository.
-	¹After Lecture 03, you'll recognize this as a static method rather than 
-	a function.
-
-*/
 public class Roller{
 	public static void main(String[] args){
+		/*
+			I was confused but I learned that .length will return the 
+			size of the array and .length() returns the size of the 
+			string
+		*/
+		if(args.length != 2){
+			System.err.println("usage: java Roll [#dice] [#sides]");
+			/*
+				I felt System.exit(0) is redundant to use when I could 
+				just else the rest of the code but thought it may be 
+				interesting to implement
+			*/
+			System.exit(0);
+		}
+		
 		int[] dieRolls = new int[Integer.parseInt(args[0])];
 		int sum = 0;
 
@@ -44,7 +35,9 @@ public class Roller{
 		for(int i : dieRolls)
 			System.out.print(i + " ");
 
+		// Print our sum and average
 		System.out.println("\nSum: " + sum);
 		System.out.println("Average: " + sum/Integer.parseInt(args[0]));
+		
 	}
 } 
