6c4c10db5ab48faaaa042502bd7285cd77d14463
Avery Strong <avery.strong@mavs.uta.edu>
2021-09-29 00:26:21 -0500

    second commit

--- e_01/Question.java
+++ e_01/Question.java
@@ -1,13 +1,33 @@
+import java.util.StringTokenizer;
 
 public class Question{
-	public Question(String question, String answer){
+	public Question(String question, String answer, Character correctAns){
 		this.question = question;
-		this.answer = answer;
+		this.correctAns = correctAns;
+
+		StringTokenizer multiTokenizer = new StringTokenizer(answer, ",");
+
+		for(int i = 0; i < 4; i++){
+			this.answers[i] = multiTokenizer.nextToken();
+		}
 	}
 	@Override
 	public String toString(){
-		return question;
+		System.out.println(question);
+
+		for(int i = 0; i < answers.length; i++){
+			System.out.print(
+				"\n"
+				+ "        "
+				+ (char)(65 + i)
+				+ ". "
+				+ answers[i]);
+		}
+		return "";
 	}
+	public Character getCorrect(){ return this.correctAns; }
+
 	private String question;
-	private String answer;
+	private Character correctAns;
+	private String[] answers = new String[4];
 }
