e970ca0894c47c8e448c55a8b6a0dc24e3a84164
Avery Strong <avery.strong@mags.uta.edu>
2021-10-18 21:42:46 -0500

    first full commit

--- P07/full_credit/gui/MainWin.java
+++ P07/full_credit/gui/MainWin.java
@@ -2,14 +2,13 @@ package gui;
 
 import java.awt.*;
 import javax.swing.*;
+import java.store.*;
 
 public class MainWin extends JFrame{
 	public MainWin(String mainWindowTitle){
 		super(mainWindowTitle); // Initialize JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Click X on close
 		setSize(400, 200);
-
-		JMenuBar menuBar = new JMenuBar();
 
 		/************************************
 					Menu Bar
@@ -18,49 +17,59 @@ public class MainWin extends JFrame{
 		// File
 		JMenu file = new JMenu("File");
 		JMenuItem quit = new JMenuItem("Quit");
-		menuBar.add(file);
 		file.add(quit);
 
 		// Create
 		JMenu create = new JMenu("Create");
 		JMenuItem donut = new JMenuItem("Donut");
 		JMenuItem java = new JMenuItem("Java");
-		menuBar.add(create);
 		create.add(donut);
 		create.add(java);
 
 		// Help
 		JMenu help = new JMenu("Help");
 		JMenuItem about = new JMenuItem("About");
-		menuBar.add(help);
 		help.add(about);
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(file);
+		menuBar.add(create);
+		menuBar.add(help);
 
 		setJMenuBar(menuBar);
 
 		/************************************
 					Tool Bar
 		************************************/
+		
+		//ImageIcon imageIcon = new ImageIcon(""); // load the image to a imageIcon
+		ImageIcon imgJava = new ImageIcon(new ImageIcon("gui/java.jpg").getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+		ImageIcon imgDonut = new ImageIcon(new ImageIcon("gui/donut.jpg").getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+
+		// Create a java icon for the toolbar
+		bJava = new JButton(imgJava);
+			bJava.setBorder(null);
+			
+		// Create a donut icon for the toolbar
+		bDonut = new JButton(imgDonut);
+			bDonut.setBorder(null);
+
 		// Add a toolbar to the region below the menu
 		JToolBar toolbar = new JToolBar();
+		toolbar.add(Box.createHorizontalStrut(25));
+		toolbar.add(bJava);
+		toolbar.add(Box.createHorizontalStrut(10));
+		toolbar.add(bDonut);
 
-		// Add a java button the toolbar
-		bJava = new JButton("java");
-			//bJava.setSize(100,100);
-			//bJava.setLayout(null);
-			//bJava.setBorder(null);
-			toolbar.add(bJava);
+		getContentPane().add(toolbar, BorderLayout.NORTH);
 
-		bDonut = new JButton("donut");
-			//bDonut.setSize(100,100);
-			//bDonut.setLayout(null);
-			//bDonut.setBorder(null);
-			toolbar.add(bDonut);
+		/************************************
+					DATA Menu
+		************************************/
 
-		add(toolbar, BorderLayout.PAGE_START);
-		
-		// Welcome to Jade
 		data = new JLabel("Welcome to Jade");
-		add(data);
+		Store store = new Store("");
+		getContentPane().add(data);
 
 		setVisible(true);
 	}
